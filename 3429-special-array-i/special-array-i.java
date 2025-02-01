e195363d1d9b5b04079c492d7d23cb72b57939b6
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2025-02-01 10:49:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (98.23%) - LeetSync

--- 3429-special-array-i/special-array-i.java
+++ 3429-special-array-i/special-array-i.java
@@ -1,3 +1,18 @@
+// class Solution {
+//     public boolean isArraySpecial(int[] nums) {
+//         if(nums.length == 1){
+//             return true;
+//         }
+
+//         for(int i = 0; i < nums.length-1; i++){
+//             if(nums[i] % 2 == nums[i+1] % 2){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean isArraySpecial(int[] nums) {
         if(nums.length == 1){
@@ -5,7 +20,7 @@ class Solution {
         }
 
         for(int i = 0; i < nums.length-1; i++){
-            if(nums[i] % 2 == nums[i+1] % 2){
+            if((nums[i] & 1) == (nums[i+1] & 1)){
                 return false;
             }
         }
