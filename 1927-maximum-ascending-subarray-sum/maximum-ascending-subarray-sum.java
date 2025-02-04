6510c0ee4b35a2578c1c430af5a8a8e287f3fc13
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2025-02-04 20:50:09 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (50.22%) - LeetSync

--- 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.java
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.java
@@ -1,3 +1,6 @@
+/*
+// Bruteforce 
+// Time complexity - O(n^2)
 class Solution {
     public int maxAscendingSum(int[] nums) {
         int n = nums.length;
@@ -14,5 +17,24 @@ class Solution {
             maxSum = Math.max(maxSum, sum);
         }
         return maxSum;
+    }
+}
+*/
+
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int n = nums.length;
+        int maxSum = Integer.MIN_VALUE;
+        int sum = nums[0];
+
+        for(int i = 1; i < n; i++){
+            if(nums[i] > nums[i-1]){
+                sum += nums[i];
+            }else{
+                maxSum = Math.max(maxSum, sum);
+                sum = nums[i];
+            }
+        }
+        return Math.max(maxSum, sum);
     }
 }
