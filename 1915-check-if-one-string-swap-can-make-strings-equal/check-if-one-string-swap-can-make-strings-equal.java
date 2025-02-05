dea0eeb6f12f5bed533c573f4e920930cfca45e3
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2025-02-05 18:08:52 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (58.39%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
         if(s1.equals(s2)) return true;
@@ -21,5 +22,45 @@ class Solution {
             }
         }
         return ((s1.charAt(firstIdx) == s2.charAt(secondIdx)) && s1.charAt(secondIdx) == s2.charAt(firstIdx));
+    }
+}
+*/
+
+// Brute force
+
+// /T.C : O(n)
+//S.C : O(26) ~= O(1)
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        int n = s1.length();
+
+        if (s1.equals(s2)) {
+            return true;
+        }
+
+        int diffs = 0;
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        for (int i = 0; i < n; i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                diffs++;
+            }
+
+            if (diffs > 2)
+                return false;
+
+            freq1[s1.charAt(i) - 'a']++;
+            freq2[s2.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
