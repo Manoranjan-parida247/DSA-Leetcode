b9ae7e3a4508e621f737d949e248c9d01b72ecf7
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2025-06-07 19:13:50 +0530

    Time: 0 ms (100.00%) | Memory: 52.9 MB (86.56%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.js
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.js
@@ -3,16 +3,16 @@
  * @return {boolean}
  */
 var check = function(nums) {
-    let n = nums.length;
+    const n = nums .length;
     let peak = 0;
 
-    for (let i = 1; i < n; i++) {
-        if (nums[i - 1] > nums[i]) {
+    for(let i = 0; i < n-1; i++){
+        if(nums[i] > nums[i+1]){
             peak++;
         }
     }
 
-    if (nums[n - 1] > nums[0]) {
+    if(nums[n-1] > nums[0]){
         peak++;
     }
 
