d2eb69515d5d3ef269326e7535ba28821d20a87b
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2025-06-21 11:16:35 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (32.95%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -2,16 +2,36 @@
  * @param {string} s
  * @return {number}
  */
-// var lengthOfLastWord = function(s) {
-//     //step 1 - trim all the spaces at the end
 
+// solution 1-> using inbuilt method
+//  var lengthOfLastWord = function(s) {
+//     s = s.trim();
+//     s = s.split(" ");
+//     return s[s.length-1].length;
+// }
 
-
-//     //step 2 - count the character till you reach the last space
-// };
-
+// solution 2 -
 var lengthOfLastWord = function(s) {
-    s = s.trim();
-    s = s.split(" ");
-    return s[s.length-1].length;
-}
+    //step 1 - trim all the spaces at the end
+    let lastNonSpaceCharIdx = -1;
+
+    for(let i = s.length-1; i >= 0 ; i--){
+        if(s[i] !== ' '){
+            lastNonSpaceCharIdx = i;
+            break;
+        }
+    }
+
+    //step 2 - count the character till you reach the last space
+    let count = 0;
+    for(let i = lastNonSpaceCharIdx; i >= 0; i--){
+        if(s[i] !== ' '){
+            count++;
+        }else{
+            break;
+        }
+    }
+    return count;
+};
+
+
