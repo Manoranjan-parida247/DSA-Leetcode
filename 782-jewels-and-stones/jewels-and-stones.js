45db73c2397258ef2659d74665476fc00cc4bf20
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2025-06-21 12:47:54 +0530

    Time: 1 ms (51.28%) | Memory: 53.8 MB (62.21%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.js
+++ 782-jewels-and-stones/jewels-and-stones.js
@@ -3,16 +3,35 @@
  * @param {string} stones
  * @return {number}
  */
+
+ //brute fprce 
+// var numJewelsInStones = function(jewels, stones) {
+//     let ans = 0;
+//     for(let i = 0; i < stones.length; i++){
+//         let currStone = stones[i];
+//         for(let j = 0; j < jewels.length; j++){
+//             if(currStone === jewels[j]){
+//                 ans++;
+//                 break;
+//             }
+//         }
+//     }
+//     return ans;
+// };
+
+
+//optimal
 var numJewelsInStones = function(jewels, stones) {
     let ans = 0;
+    const set = new Set();
+    for(let i = 0; i < jewels.length; i++){
+        set.add(jewels[i]);
+    }
+
     for(let i = 0; i < stones.length; i++){
-        let currStone = stones[i];
-        for(let j = 0; j < jewels.length; j++){
-            if(currStone === jewels[j]){
-                ans++;
-                break;
-            }
+        if(set.has(stones[i])){
+            ans++;
         }
     }
     return ans;
-};
+}
