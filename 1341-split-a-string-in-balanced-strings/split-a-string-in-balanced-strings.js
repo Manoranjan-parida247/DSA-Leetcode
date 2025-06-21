0b56168eb1000b664de78108e55df9263f0b3c3a
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2025-06-21 17:38:11 +0530

    Time: 1 ms (42.86%) | Memory: 53.8 MB (52.29%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.js
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.js
@@ -2,26 +2,39 @@
  * @param {string} s
  * @return {number}
  */
+// var balancedStringSplit = function (s) {
+//     const n = s.length;
+//     let lCount = 0;
+//     let rCount = 0;
+
+//     let ans = 0;
+
+//     for (let i = 0; i < n; i++) {
+//         if (s[i] === 'L') {
+//             lCount++;
+//         } else {
+//             rCount++;
+//         }
+
+//         if (lCount === rCount) {
+//             ans++;
+//             lCount = 0;
+//             rCount = 0;
+//         }
+//     }
+
+//     return ans;
+// };
+
 var balancedStringSplit = function (s) {
-    const n = s.length;
-    let lCount = 0;
-    let rCount = 0;
-
     let ans = 0;
+    let count = 0;
+    const n = s.length;
 
-    for (let i = 0; i < n; i++) {
-        if (s[i] === 'L') {
-            lCount++;
-        } else {
-            rCount++;
-        }
-
-        if (lCount === rCount) {
-            ans++;
-            lCount = 0;
-            rCount = 0;
-        }
+    for(const char of s){
+        char === 'L' ? count++ : count-- ;
+        
+        if(count === 0) ans++;
     }
-
     return ans;
-};
+}
