6808451ff1a2f35db9e1baea44929cc391b22f81
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2025-06-21 11:51:49 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (40.79%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -10,28 +10,42 @@
 //     return s[s.length-1].length;
 // }
 
-// solution 2 -
+// solution 2 - two loop
+// var lengthOfLastWord = function(s) {
+//     //step 1 - trim all the spaces at the end
+//     let lastNonSpaceCharIdx = -1;
+
+//     for(let i = s.length-1; i >= 0 ; i--){
+//         if(s[i] !== ' '){
+//             lastNonSpaceCharIdx = i;
+//             break;
+//         }
+//     }
+
+//     //step 2 - count the character till you reach the last space
+//     let count = 0;
+//     for(let i = lastNonSpaceCharIdx; i >= 0; i--){
+//         if(s[i] !== ' '){
+//             count++;
+//         }else{
+//             break;
+//         }
+//     }
+//     return count;
+// };
+
+
+// solution 3 -> 1 loop
 var lengthOfLastWord = function(s) {
-    //step 1 - trim all the spaces at the end
-    let lastNonSpaceCharIdx = -1;
-
-    for(let i = s.length-1; i >= 0 ; i--){
-        if(s[i] !== ' '){
-            lastNonSpaceCharIdx = i;
-            break;
-        }
-    }
-
-    //step 2 - count the character till you reach the last space
+    const n = s.length;
     let count = 0;
-    for(let i = lastNonSpaceCharIdx; i >= 0; i--){
+    for(let i = n-1; i >= 0; i--){
         if(s[i] !== ' '){
             count++;
-        }else{
+        }else if(s[i] === ' ' && count > 0){
             break;
         }
+        
     }
     return count;
-};
-
-
+}
