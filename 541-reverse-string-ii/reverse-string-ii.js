7cd818bba25151aa8c0b767fcbcb3f918eb2865e
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2025-06-22 15:24:28 +0530

    Time: 4 ms (60.41%) | Memory: 55.8 MB (81.41%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.js
+++ 541-reverse-string-ii/reverse-string-ii.js
@@ -3,23 +3,27 @@
  * @param {number} k
  * @return {string}
  */
-var reverseStr = function(s, k) {
+var reverseStr = function (s, k) {
     let arr = s.split('');
 
-    for(let i = 0; i < s.length; i += 2 *k  ){
+    for (let i = 0; i < s.length; i += 2 * k) {
         // Determine the end index for reversal
         let left = i;
-        let right = Math.min(i+k-1, arr.length-1);
+        let right = Math.min(i + k - 1, arr.length - 1);
 
         // Reverse characters between left and right
+        swap(arr, left, right);
 
-        while(left < right){
-            const temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++;
-            right--;
-        }
     }
     return arr.join('');
 };
+
+const swap = (arr, left, right) => {
+    while (left < right) {
+        const temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+}
