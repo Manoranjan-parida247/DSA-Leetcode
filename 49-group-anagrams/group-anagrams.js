58a85d8d9460fc328768755ec7fd3f14dc791ec8
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2025-06-23 01:33:44 +0530

    Time: 30 ms (70.04%) | Memory: 67.5 MB (63.04%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+
+//  ✅ Code (Sorted Version):
+var groupAnagrams = function(strs) {
+    const map = new Map();
+
+    for(let str of strs){
+        // Sort characters of the string to form the key
+        const sortedStr = str.split('').sort().join('');
+
+        if(!map.has(sortedStr)){
+            map.set(sortedStr, []);
+        }
+
+        map.get(sortedStr).push(str);
+    }
+
+    return Array.from(map.values());
+};
