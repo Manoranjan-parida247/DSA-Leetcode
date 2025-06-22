f5e7dbf1a6a68b75f71b888701d1ffe63c439d0b
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2025-06-23 02:01:03 +0530

    Time: 25 ms (86.25%) | Memory: 65.9 MB (94.14%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -4,21 +4,44 @@
  */
 
 //  ✅ Code (Sorted Version):
-var groupAnagrams = function(strs) {
-    const map = new Map();
+// var groupAnagrams = function(strs) {
+//     const map = new Map();
 
-    for(let str of strs){
-        // Sort characters of the string to form the key
-        const sortedStr = str.split('').sort().join('');
+//     for(let str of strs){
+//         // Sort characters of the string to form the key
+//         const sortedStr = str.split('').sort().join('');
 
-        if(!map.has(sortedStr)){
-            map.set(sortedStr, [str]);
-        }else{
-            map.get(sortedStr).push(str);
-        }
+//         if(!map.has(sortedStr)){
+//             map.set(sortedStr, [str]);
+//         }else{
+//             map.get(sortedStr).push(str);
+//         }
 
         
-    }
+//     }
 
-    return Array.from(map.values());
-};
+//     return Array.from(map.values());
+// };
+
+
+//  ✅ Code (without key Sorted Version):
+var groupAnagrams = function(strs) {
+    const mp = new Map();
+
+    for(let str of strs){
+        const freqArr = new Array(26).fill(0);
+
+        for(let ch of str){
+            freqArr[ch.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+        }
+
+        let key = freqArr.join('#');
+
+        if(!mp.has(key)){
+            mp.set(key, [str]);
+        }else{
+            mp.get(key).push(str);
+        }
+    }
+    return Array.from(mp.values());
+}
