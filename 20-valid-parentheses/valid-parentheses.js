971ae479bdc33359f27034e59e346811f154cc88
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2025-06-27 00:55:25 +0530

    Time: 3 ms (46.39%) | Memory: 56.2 MB (41.93%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack = [];
+        
+        for(let i = 0; i < s.length; i++){
+            const char = s[i];
+            
+            if(char === '(' || char === '[' || char === '{'){
+                stack.push(char);
+            }else if(char === ')' || char === ']' || char === '}'){
+                if(stack.length === 0) return false;
+                
+                const top = stack.pop();
+                
+                if((char === ')' && top !== '(') ||
+                (char === ']' && top !== '[') ||
+                (char === '}' && top !== '{')){
+                    return false;
+                }
+                
+            }
+        }
+        
+        return stack.length === 0 ? true : false;
+};
