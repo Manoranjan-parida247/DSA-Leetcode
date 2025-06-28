714fb722abd537d228ca5cb31b355560d436c2f3
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2025-06-28 17:13:51 +0530

    Time: 46 ms (85.77%) | Memory: 58.2 MB (89.66%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -10,15 +10,31 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+// var hasCycle = function(head) {
+//     let curr = head;
+//     const seenNodes = new Set();
+//     while(head !== null){
+//         if(seenNodes.has(head)){
+//             return true;
+//         }
+//         seenNodes.add(head);
+//         head = head.next;
+//     }
+//     return false;
+// };
+
 var hasCycle = function(head) {
-    let curr = head;
-    const seenNodes = new Set();
-    while(head !== null){
-        if(seenNodes.has(head)){
-            return true;
-        }
-        seenNodes.add(head);
-        head = head.next;
+    if(head === null) return false;
+    let slow = head;
+    let fast = head.next;
+
+    while(slow !== fast){
+
+        if(fast === null || fast.next === null) return false;
+
+        slow = slow.next;
+        fast = fast.next.next;
     }
-    return false;
-};
+
+    return true;
+}
