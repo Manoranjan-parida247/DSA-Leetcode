7a4431a21f77d4657f62a40e7c87b84c26ab607d
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2025-07-21 22:45:01 +0530

    Time: 52 ms (87.50%) | Memory: 65.1 MB (80.21%) - LeetSync

---
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const makeFancyString = s => {
+    let result = '';
+    let last = s[0];
+    let count = 1;
+
+    result += last;
+
+    for (let i = 1; i < s.length; i++) {
+        if (s[i] !== last) {
+            last = s[i];
+            count = 0;
+        }
+
+        if (++count > 2) continue;
+
+        result += s[i];
+    }
+
+    return result;
+};
