733e01e0295814ef0a6705326d401024a01061bd
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2025-08-22 23:25:44 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (52.61%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
@@ -1,21 +1,42 @@
+// class Solution {
+//     public int balancedStringSplit(String s) {
+//         int lCount = 0;
+//         int rCount = 0;
+//         int ans = 0;
+
+//         for (int i = 0; i < s.length(); i++) {
+//             char ch = s.charAt(i);
+//             if (ch == 'L') {
+//                 lCount++;
+//             } else {
+//                 rCount++;
+//             }
+
+//             if (lCount == rCount) {
+//                 ans++;
+//                 lCount = 0;
+//                 rCount = 0;
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int balancedStringSplit(String s) {
-        int lCount = 0;
-        int rCount = 0;
         int ans = 0;
+        int count = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+        for (char ch : s.toCharArray()) {
             if (ch == 'L') {
-                lCount++;
+                count++;
             } else {
-                rCount++;
+                count--;
             }
 
-            if (lCount == rCount) {
+            if (count == 0) {
                 ans++;
-                lCount = 0;
-                rCount = 0;
             }
         }
 
