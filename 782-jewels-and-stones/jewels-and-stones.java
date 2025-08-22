66afee9b236f057fd55105e466c1dce0984f3111
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2025-08-22 22:52:17 +0530

    Time: 1 ms (58.93%) | Memory: 41.8 MB (79.08%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.java
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -1,15 +1,36 @@
+// class Solution {
+//     public int numJewelsInStones(String jewels, String stones) {
+//         int ans = 0;
+//         for (int i = 0; i < stones.length(); i++) {
+//             char currStone = stones.charAt(i);
+//             for (int j = 0; j < jewels.length(); j++) {
+//                 if (currStone == jewels.charAt(j)) {
+//                     ans++;
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         int ans = 0;
+        Set<Character> set = new HashSet<>();
+
+        // Add jewels into the set
+        for (int i = 0; i < jewels.length(); i++) {
+            set.add(jewels.charAt(i));
+        }
+
+        // Count stones that are jewels
         for (int i = 0; i < stones.length(); i++) {
-            char currStone = stones.charAt(i);
-            for (int j = 0; j < jewels.length(); j++) {
-                if (currStone == jewels.charAt(j)) {
-                    ans++;
-                    break;
-                }
+            if (set.contains(stones.charAt(i))) {
+                ans++;
             }
         }
+
         return ans;
     }
 }
