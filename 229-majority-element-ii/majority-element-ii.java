4f97ff0eb45c2624bc4bf88d4690ad1dd6a9e818
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2025-10-27 13:28:40 +0530

    Time: 3 ms (74.40%) | Memory: 50.4 MB (56.21%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,18 +1,45 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
-        Map<Integer, Integer> mp = new HashMap<>();
+        int mini = n/3;
         List<Integer> res = new ArrayList<>();
-        int mini = n/3 + 1;
-        
-        for(int i = 0; i < n; i++){
-            mp.put(nums[i], mp.getOrDefault(nums[i], 0)+1);
-            
-            if(mp.get(nums[i])  == mini){
-                res.add(nums[i]);
-            }
 
-            if(res.size() == 2) break;
+        int el1 = Integer.MIN_VALUE;
+        int el2 = Integer.MIN_VALUE;
+        int cnt1 = 0;
+        int cnt2 = 0;
+
+        for(int i = 0; i < n; i++){
+            if(cnt1 == 0 && nums[i] != el2){
+                cnt1++;
+                el1 = nums[i];
+            }else if(cnt2 == 0 && nums[i] != el1){
+                cnt2++;
+                el2 = nums[i];
+            }else if(nums[i] == el1){
+                cnt1++;
+            }else if(nums[i] == el2){
+                cnt2++;
+            }else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        cnt1 = 0;
+        cnt2 = 0;
+
+        for(int num : nums){
+            if(num == el1) cnt1++;
+            if(num == el2) cnt2++;
+        }
+
+        if(cnt1 > mini){
+            res.add(el1);
+        }
+
+        if(cnt2 > mini && el2 != el1){
+            res.add(el2);
         }
 
         return res;
