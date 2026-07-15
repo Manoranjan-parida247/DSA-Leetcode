162c6e5908987fadf61e08c9d7c1c2a419f44ab3
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2026-07-15 21:05:21 +0530

    Time: 31 ms (83.37%) | Memory: 60 MB (17.04%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,34 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // Opitmal
-        // List to store the triplets that sum up to target
-        List<List<Integer>> res = new ArrayList<>();
         int n = nums.length;
-
-        //sort the array
         Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n-2; i++){
             //skip duplicates
             if(i > 0 && nums[i] == nums[i-1]) continue;
 
-            // Two pointers approach
+            //two pointer
             int j = i+1;
             int k = n-1;
 
             while(j < k){
                 int sum = nums[i] + nums[j] + nums[k];
-                if(sum > 0){
-                    k--;
-                }else if(sum < 0){
+                if(sum < 0){
                     j++;
+                }else if(sum > 0){
+                    k--;
                 }else{
-                    // Found a triplet that sums up to target
                     List<Integer> temp = new ArrayList<>();
                     temp.add(nums[i]);
                     temp.add(nums[j]);
                     temp.add(nums[k]);
-                    res.add(temp);
+
+                    ans.add(temp);
 
                     //skip duplicates
                     j++;
@@ -39,6 +35,6 @@ class Solution {
             }
         }
 
-        return res;
+        return ans;
     }
 }
