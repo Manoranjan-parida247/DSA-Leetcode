f9313b24f71a02227f797c06c7474cd475696bc3
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2026-07-15 19:14:45 +0530

    Time: 1 ms (62.75%) | Memory: 43.3 MB (32.91%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,34 +1,22 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        // Edge case: if the array is empty or the first string is empty
-        if (strs.length == 0 || strs[0].length() == 0) {
-            return "";
-        }
-
-        // Use StringBuilder for efficient string concatenation
+        Arrays.sort(strs);
+        String start = strs[0];
+        String end = strs[strs.length-1];
+        int i = 0; //start
+        int j = 0; //end
         StringBuilder ans = new StringBuilder();
 
-        // Loop through each character of the first string
-        for (int i = 0; i < strs[0].length(); i++) {
-            // Get the current character from the first string
-            char currentChar = strs[0].charAt(i);
-
-            // Compare this character with the same position in all other strings
-            for (int j = 1; j < strs.length; j++) {
-                // If:
-                // 1. Current index exceeds the length of any string, OR
-                // 2. The character at this index doesn't match
-                if (i >= strs[j].length() || strs[j].charAt(i) != currentChar) {
-                    // Return the prefix found so far
-                    return ans.toString();
-                }
+        while(i < start.length() && j < end.length()){
+            if(start.charAt(i) == end.charAt(j)){
+                ans.append(start.charAt(i));
+                i++;
+                j++;
+            }else{
+                break;
             }
-
-            // If all strings had the same character at this position, add it to the answer
-            ans.append(currentChar);
         }
 
-        // Return the final longest common prefix
         return ans.toString();
     }
 }
