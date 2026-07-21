534e003d0ce65430ed9fd4a87022feea8726235f
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2026-07-21 17:07:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (52.36%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,22 +1,16 @@
 class Solution {
     public int fib(int n) {
-        if(n <= 1) return n;
+        if (n <= 1) return n;
 
-        int[] dp = new int[n+1]; 
-        Arrays.fill(dp, -1);  
+        int prev2 = 0;
+        int prev1 = 1;
 
-        return helper(n, dp);
-    }
-
-    private int helper(int n, int[] dp){
-        if(n <= 1) return n;
-        
-        if(dp[n] != -1){
-            return dp[n];
+        for (int i = 2; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
 
-        dp[n] = helper(n-1, dp) + helper(n-2, dp);
-
-        return dp[n];
+        return prev1;
     }
 }
