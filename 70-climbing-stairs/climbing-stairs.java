2097a8b8f754399bc95791ae34c0261e7b59e683
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2026-07-22 22:54:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (17.49%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -5,7 +5,11 @@ class Solution {
         dp[0] = 1;
         dp[1] = 1;
 
-        return helper(dp, n);
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
     public int helper(int[] dp, int n){
         if(dp[n] != -1){
