5b701f6189397cdf77e58b0e2654203d073a8b7b
Manoranjan Parida <134356080+Manoranjan-parida247@users.noreply.github.com>
2026-07-26 11:14:56 +0530

    Time: 1 ms (99.96%) | Memory: 46.6 MB (96.52%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,38 +1,11 @@
-// Question Link -> https://leetcode.com/problems/single-number/
-
-
-// Time complexity -> O(N);
-// Space Complexity -> O(N)
-/*
 class Solution {
     public int singleNumber(int[] nums) {
-        int n = nums.length;
-        HashMap<Integer, Integer> mp = new HashMap<>();
+        int xor = 0;
 
-        for(int i = 0; i < n; i++){
-            mp.put(nums[i], mp.getOrDefault(nums[i], 0)+1);
+        for(int num: nums){
+            xor ^= num;
         }
 
-        for(Map.Entry<Integer, Integer> it: mp.entrySet()){
-            if(it.getValue() == 1){
-                return it.getKey();
-            }
-        }
-        return -1;
-    }
-}
-*/
-// Time complexity -> O(N);
-// Space Complexity -> O(1)
-
-class Solution {
-    public int singleNumber(int[] nums) {
-        int res = 0;
-
-        for(int num : nums){
-            res = res ^ num;
-        }
-
-        return res;
+        return xor;
     }
 }
